d8098522ba2f4aa9f8323d7bf75877ac29213c38
Manik-Arora <manikarora2605@gmail.com>
2024-06-13 21:59:04 +0530

    Adding register and login route

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,14 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES UPLOAD*/
+app.post("/auth/register", upload.single("picture"), register);
+
+app.get("/", (req, res) => [res.send("Welcome, Home!")]);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
