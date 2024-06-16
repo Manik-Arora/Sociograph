fe26b07c0607a3952106cdea315f476211d90a8d
Manik-Arora <manikarora2605@gmail.com>
2024-06-16 08:25:32 +0530

    Added users route

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -45,6 +47,7 @@ app.get("/", (req, res) => [res.send("Welcome, Home!")]);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
