eebc34d7ec01f75b2824556decd313a09b38c337
Manik-Arora <manikarora2605@gmail.com>
2024-06-20 10:13:36 +0530

    Adding navbar component

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,12 +1,5 @@
-import { Box } from "@mui/material";
-import Navbar from "scenes/navbar";
-
 const HomePage = () => {
-  return (
-    <Box>
-      <Navbar />
-    </Box>
-  );
+  return <div>Homepage</div>;
 };
 
 export default HomePage;
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+const intialState = {
   mode: "light",
   user: null,
   token: null,
@@ -15,12 +15,10 @@ export const authSlice = createSlice({
       state.mode = state.mode === "light" ? "dark" : "light";
     },
     setLogin: (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
+      (state.user = action.payload.user), (state.token = action.payload.token);
     },
     setLogout: (state) => {
-      state.user = null;
-      state.token = null;
+      (state.user = null), (state.token = null);
     },
     setFriends: (state, action) => {
       if (state.user) {
