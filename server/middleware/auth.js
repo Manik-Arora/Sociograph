38bb392be6c5f7a6088a1fbbac59c402a643e6ce
Manik-Arora <manikarora2605@gmail.com>
2024-06-23 12:28:09 +0530

    Adding widgets for home page

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,9 +2,8 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
   try {
-    const token = req.header("Authorization");
+    let token = req.header("Authorization");
     if (!token) return res.status(403).send("Access Denied");
-
     if (token.startsWith("Bearer ")) {
       token = token.slice(7, token.length).trimLeft();
     }
